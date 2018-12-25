e80aac78bf429cce92b8b63050eb250a35db94c0
pengpeijie <pengpeijie@cvte.com>
2018-12-25 10:38:05 +0800

    sample : 调用固定返回值方法，便于注入测试

--- sample/src/main/java/com/pengpj/FirstController.java
+++ sample/src/main/java/com/pengpj/FirstController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Calendar;
-
 /**
  * @author pengpj
  * @date 2018/12/24
@@ -16,6 +14,10 @@ public class FirstController {
 
     @GetMapping("/now")
     public Object now() {
-        return Calendar.getInstance().getTimeInMillis();
+        return get();
+    }
+
+    private String get() {
+        return "pengpj";
     }
 }
